34a5a3b61ea19eb179159793af92295b6fcf6cd6
knhraina <knhraina00@gmail.com>
2023-08-05 20:22:45 +0900

    클린레시피 이전까지

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <div className='background'>
+      <Header/>
+      <Main/>
+      <Footer/>
+      </div>
+    </div>
+
   );
 }
 
